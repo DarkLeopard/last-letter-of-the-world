c6ee7f086303aadcb625ec8e97a0e0dd628c0cd0
Leo <darkleopard3@ya.ru>
2021-08-13 21:06:13 +0300

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {FormControl} from "@angular/forms";
+import {BehaviorSubject} from "rxjs";
+
+interface IDict {
+  [key: string]: {
+    definition: string;
+  };
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'last-letter-of-the-world';
+  public isGameNew$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+  public dict: IDict;
+
+  public playerWord: FormControl = new FormControl(undefined);
+  public pcWord: FormControl = new FormControl({value: undefined, disabled: true});
+
+  constructor(
+    private http: HttpClient,
+  ) {
+    this.http.get('./assets/dict/russian_nouns_with_definition.json')
+      .subscribe((dict: IDict) => {
+        this.dict = dict;
+        this.pcTurn();
+      })
+  }
+
+  public get lastLetter(): string {
+    return (this.pcWord.value as string).split('').reverse()[0];
+  }
+
+  public startGame(): void {
+    this.isGameNew$.next(false);
+  }
+
+  public playerTurn(): void {
+    const newWord: string | undefined = this.playerWord.value;
+    if (this.lastLetter !== newWord.split('')[0]) {
+      this.playerWord.setErrors({notLastLetter: true});
+    } else {
+      this.pcTurn();
+    }
+  }
+
+  private pcTurn(): void {
+    const lastLetter: string = (this.playerWord.value as string)?.split('')?.reverse()?.[0] || Object.keys(this.dict)[0][0];
+    const newWord = this.shuffleArr(
+      Object.keys(this.dict)
+        .filter((word) => {
+          return word.split('')[0] === lastLetter;
+        })
+    )[0];
+    this.pcWord.setValue(newWord);
+  }
+
+  private shuffleArr(array: string[]): string[] {
+    let currentIndex = array.length, randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClientModule} from "@angular/common/http";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
